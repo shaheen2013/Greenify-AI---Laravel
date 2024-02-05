2a9e0a7c5177697b0ab678e86fd0f629fc4de85d
shaheen2013-ShahriarRahat <coredeveloper.2013@gmail.com>
2024-02-05 20:05:50 +0600

    phprets

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,26 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('test', function () {
+    $start_date = now()->subDay()->format('Y-m-d');
+    $end_date = now()->addDay()->format('Y-m-d');
+    $dateRange='ListDate = '. $start_date. ' - '. $end_date;
+
+    $config = new \PHRETS\Configuration;
+    $config->setLoginUrl('https://r_idx.gsmls.com/rets_idx/login.do')
+            ->setUsername('Step1Solutions')
+            ->setPassword('9RGKY8EFISQ9')
+            ->setUserAgent('Step1Solutions/1.0')
+            ->setUserAgentpassword('Q3TWmp4')
+            ->setRetsVersion('1.7.2');
+
+    $rets = new \PHRETS\Session($config);
+    dd($rets->Login());
+
+    $connect = $rets->Login();
+
+    $results = $rets->Search('Property', 'RES', $dateRange, ['MLStatus' => 'A', 'MLStatus' => 'US', 'MLStatus' => 'CS']);
+
+    dd($connect, $results);
+});
